94b5c9c47d237aa81acfc2e58ca6470c8c3cdd62
Darwin <12121275@student.tuwien.ac.at>
2023-02-24 20:37:30 +0100

    Add Hero Section

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import React, { FC, ReactNode } from "react";
 import "./App.css";
 import Layout from "@components/Layout";
 import Home from "@pages/Home";
-import About from "@pages/About";
-import Dashboard from "@pages/Dashboard";
+import Api from "@pages/Api";
 import NotFound from "@pages/NotFound";
 import { Routes, Route, Outlet, Link } from "react-router-dom";
+import Grid from "@pages/Grid";
 
 const App = () => {
   return (
@@ -13,12 +13,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="dashboard" element={<Dashboard />} />
-
-          {/* Using path="*"" means "match anything", so this route
-                acts like a catch-all for URLs that we don't have explicit
-                routes for. */}
+          <Route path="grid" element={<Grid />} />
+          <Route path="api" element={<Api />} />
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,19 @@
-import React, { FC } from 'react'
+import React, { FC } from "react";
 
-interface HomeProps {
-  
-}
+interface HomeProps {}
 
-const Home: FC<HomeProps> = ({  }) => {
+const Home: FC<HomeProps> = ({}) => {
   return (
-    <div>
-     Home
+    <div className="">
+      <div className="relative">
+        <img className="opacity-80" src="HeroImage.jpg" alt="HeroImage" />
+      </div>
+      <div className="absolute text-zinc-100 font-semibold bottom-3/4 right-1/4 md:bottom-2/3 md:right-2/4 ml-10  text-2xl lg:text-4xl md:text-3xl tracking-wide">
+        Success is not final, failure is not fatal: it is the courage to
+        continue that counts
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
